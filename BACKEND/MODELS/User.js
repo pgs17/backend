5ebab45f44ed8f5422d7ec0b7691c93833cfd844
PGS <sarangipriyanshu@gmail.com>
2023-04-09 16:45:33 +0530

    just for me

---
+++ BACKEND/MODELS/User.js
@@ -0,0 +1,26 @@
+// in this we will make a user schema and  out of schema we will model it
+// mongoose.schema object is used 
+
+const mongoose = require("mongoose")
+
+
+// schema creation
+const UserSchema = new mongoose.Schema({ 
+name:{
+    type: String,
+    required: true
+},
+age:{
+    type: Number,
+    required: true
+},
+UserName: {
+    type: String,
+    required: true
+}
+});
+
+// schema model takes two input schema name and the collection or table name in file 
+const UserModel = mongoose.model("users", UserSchema)
+
+module.exports=UserModel;
---
+++ BACKEND/index.js
@@ -0,0 +1,43 @@
+// in backend here we will write our api setup and database setup and before dependencies install like express mongoose and cors(for connection between frontend and backend)
+const express = require("express") // import express library
+const app = express() // this app variable is used to acess all express functions
+
+const corse = require("cors") // to connect frontend to backend
+app.use(corse())
+ 
+app.use(express.json()); // if we dont do this then json body request gives error as its json
+
+const mongoose = require("mongoose")
+const UserModel = require("./MODELS/User")
+mongoose.connect( "mongodb+srv://sarangipriyanshu:Kanha%402003@cluster0.qpmdgyx.mongodb.net/MERNTUTORIAL?retryWrites=true&w=majority" )
+
+// request data to backend or give it using express 
+app.get("/getusers", async (request , response) => {
+    try{
+         const users= await UserModel.find();
+         response.send(users); 
+    }
+    catch{
+        response.status(500).json();
+    }
+})
+
+
+// add data to database 
+app.post("/adduser", async(request , response)=> {
+                  const {name,age,Username} =request.body;
+                  const newuser = new UserModel({name,age,Username});
+                  await newuser.save();
+                  response.send(newuser);
+                   
+});
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log("SERVER RUNS PERFECTLY!")
+}) // to make our api start we used this function takes port no as locally run and a call back function to show our server runs perfectly 
