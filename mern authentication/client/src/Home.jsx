20cd64db78b52320f8f77d5f3abae8359e152d3e
Priyanshu Sarangi <sarangipriyanshu@gmail.com>
2023-04-24 19:40:54 +0530

    yes

--- mern authentication/client/src/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div>
-     <p> This is Home Page. Kindly login if old user or else register usrself if u are new</p>
-    </div>
-  )
-}
